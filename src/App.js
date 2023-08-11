be32d63adb6957ea16e35fa9dd93b4108fc961a3
dpurcell <dpurcell.pd@gmail.com>
2023-08-10 21:53:09 -0400

    Calculator functions

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import './App.css';
 import {useState, useRef} from "react";
 import Calculator from './components/Calculator';
 
-export default function App() {
+export default function App() {  
+  const assignmentRef = useRef();
+  const [assignment, setAssignment] = useState(0);
   return (
     <div className="container">
-      <Calculator />          
+      <Calculator assignmentRef={assignmentRef} />     
+     
     </div>
   );
 }
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, {useRef} from 'react'
 
+const add = e => {
+    e.preventDefault();
+}
 
-export default function Calculator() {
+const subtract = e => {
+    e.preventDefalt();
+}
+
+const multiply = e => {
+    e.preventDefalt();
+}
+
+const divide = e => {
+    e.preventDefault();
+}
+
+export default function Calculator(props) {   
   return (
     <div className="calculator">
         <h3>React Currency Calculator</h3>
+        <form>
+            
+        </form>
         
     </div>
   )
