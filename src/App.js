4799117b1517525acf52544644f9601be6b8ba04
dpurcell <dpurcell.pd@gmail.com>
2023-08-14 07:58:39 -0400

    updated App / Calculator components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import {useState, useRef} from "react"; 
 import Calculator from "./components/Calculator";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {  
   return ( 
-    <div className="App"> 
-      <Calculator />
-    </div> 
+    <>    
+      <Calculator />    
+      <Footer />     
+    </>
   ); 
 } 
  
