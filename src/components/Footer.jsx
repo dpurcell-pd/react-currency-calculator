5eef6ae6e9c4f2b6b684dee96112d28f99c6c81e
dpurcell <dpurcell.pd@gmail.com>
2023-08-14 06:51:23 -0400

    added Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className="footer">;
+        <h1>Instructions</h1>          
+        <br /> 
+    </div>
+  )
+}
+
